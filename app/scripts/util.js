4f75e2b3e87559f1486e2e21f8ce05c1fcc422e3
Raptis Dimos <raptis.dimos@yahoo.gr>
2016-04-11 19:27:38 +0300

    removed unnecessary 'strict' mode

--- app/scripts/util.js
+++ app/scripts/util.js
@@ -27,7 +27,6 @@ function parseURL(url) {
 
 /* Function for escaping text, including HTML symbols */
 var escapeHtml = (function () {
-    'use strict';
     var chr = { '"': '&quot;', '&': '&amp;', '<': '&lt;', '>': '&gt;' };
     return function (text) {
         return text.replace(/[\"&<>]/g, function (a) { return chr[a]; });
