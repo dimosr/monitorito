d98a73cfa9fcc830b8f4487e0df713c7fb67cb16
Raptis Dimos <raptis.dimos@yahoo.gr>
2016-07-04 23:52:24 +0100

    Added regression test for nested clustering

--- test/scripts/graph/cluster/ClusteringEngine.js
+++ test/scripts/graph/cluster/ClusteringEngine.js
@@ -1,42 +1,21 @@
 QUnit.module( "graph.cluster.ClusteringEngine", {
 	
 	beforeEach: function() {
-		this.graph = sinon.createStubInstance(Graph);
+		this.visualisationNetwork = new VisualisationNetwork(jQuery("<canvas>")[0]);
+		this.graph = new Graph(this.visualisationNetwork);
+		this.graph.createDomainNode("www.example.com");
+		this.graph.createDomainNode("another.example.com");
+		this.graph.createDomainNode("test.com");
+		this.graph.createDomainNode("dummy.com");
+		this.graph.createDomainNode("test.co.uk");
+		//this.graph = sinon.createStubInstance(Graph);
 
 		this.clusteringEngine = new ClusteringEngine(this.graph);
 	}
 });
 
-QUnit.test("clustering functionalities", function(assert) {
+QUnit.test("Cluster creation and deletion work successfully", function(assert) {
 	var clusteringEngine = this.clusteringEngine;
-
-	var node1 = sinon.createStubInstance(DomainNode);
-	node1.getDomain.returns("www.example.com");
-	node1.getID.returns("www.example.com");
-	node1.getOutgoingDomainEdges.returns([]);
-	node1.getIncomingDomainEdges.returns([]);
-	var node2 = sinon.createStubInstance(DomainNode);
-	node2.getDomain.returns("another.example.com");
-	node2.getID.returns("another.example.com");
-	node2.getOutgoingDomainEdges.returns([]);
-	node2.getIncomingDomainEdges.returns([]);
-	var node3 = sinon.createStubInstance(DomainNode);
-	node3.getDomain.returns("test.com");
-	node3.getID.returns("test.com");
-	node3.getOutgoingDomainEdges.returns([]);
-	node3.getIncomingDomainEdges.returns([]);
-	var node4 = sinon.createStubInstance(DomainNode);
-	node4.getDomain.returns("dummy.com");
-	node4.getID.returns("dummy.com");
-	node4.getOutgoingDomainEdges.returns([]);
-	node4.getIncomingDomainEdges.returns([]);
-	var node5 = sinon.createStubInstance(DomainNode);
-	node5.getDomain.returns("test.co.uk");
-	node5.getID.returns("test.co.uk");
-	node5.getOutgoingDomainEdges.returns([]);
-	node5.getIncomingDomainEdges.returns([]);
-	
-	this.graph.getDomainNodes.returns([node1, node2, node3, node4]);
 
 	clusteringEngine.clusterByDomain(["example.com", "test.com"], "cluster-1");
 
@@ -44,14 +23,25 @@ QUnit.test("clustering functionalities", function(assert) {
 	assert.ok(cluster != null, "Cluster was successfully created.");
 	assert.equal(cluster.getNodes().length, 3, "All nodes have been successfully clustered");
 
+	clusteringEngine.deCluster("cluster-1");
+	var cluster = clusteringEngine.getCluster("cluster-1");
+	assert.ok(cluster == null, "Cluster was successfully deleted.");
+});
+
+QUnit.test("Error thrown when attempting to create cluster with existing ID", function(assert) {
+	var clusteringEngine = this.clusteringEngine;
+	clusteringEngine.clusterByDomain(["example.com", "test.com"], "cluster-1");
 	assert.throws(
 		function() {
 			clusteringEngine.clusterByDomain(["example.com", "dummy.com"], "cluster-1");
-		}, 
+		},
 		Error,
 		"cannot create cluster with existing ID"
 	);
+});
 
+QUnit.test("Error thrown when attempting to create cluster with only 1 contained node", function(assert) {
+	var clusteringEngine = this.clusteringEngine;
 	assert.throws(
 		function() {
 			clusteringEngine.clusterByDomain(["test"], "cluster-2");
@@ -59,8 +49,17 @@ QUnit.test("clustering functionalities", function(assert) {
 		Error,
 		"cannot create cluster with only 1 containing node"
 	);
+});
 
-	clusteringEngine.deCluster("cluster-1");
-	var cluster = clusteringEngine.getCluster("cluster-1");
-	assert.ok(cluster == null, "Cluster was successfully deleted.");
+QUnit.test("Clustering already clustered domain throws Error", function(assert) {
+	var clusteringEngine = this.clusteringEngine;
+
+	clusteringEngine.clusterByDomain(["example.com", "test.com"], "cluster-1");
+	assert.throws(
+		function() {
+			clusteringEngine.clusterByDomain(["example.com", "dummy.com"], "cluster-2");
+		},
+		Error,
+		"cannot create nested clustering"
+	);
 });
